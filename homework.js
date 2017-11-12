350c5075e237e70c329eaf6c3506e6fbc1757838
Pankaj Singh <pankaj28843@gmail.com>
2017-11-12 21:58:11 +0100

    Step 3: define a function to fetch JSON data

--- homework.js
+++ homework.js
@@ -1,3 +1,37 @@
+function fetchData(url, callbackFunction) {
+    // Create new ajax call with the js function called XMLHttpRequest
+    const req = new XMLHttpRequest();
+
+    req.addEventListener('load', function (data) {
+        // This in here is our callback function
+        // Check our server responsecode, 200 means ok, success: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+        if (this.status === 200) {
+            const responseText = req.responseText;
+            const json = JSON.parse(responseText);
+
+            // Call the callback function with json data
+            callbackFunction(json);
+        } else {
+            console.log('Something is probably wrong with the url');
+        }
+    });
+
+    req.addEventListener('error', function () {
+        console.log('Server error like timeout');
+    });
+
+    // initializes a request with an http method
+    req.open("GET", url);
+
+    // Sends the request
+    req.send();
+}
+
+
+fetchData('https://api.github.com/orgs/HackYourFuture/repos', function(data){
+    console.log(data);
+});
+
 const myButton = document.querySelector('#myButton');
 
 const myButtonClickHandler = function () {
@@ -5,3 +39,5 @@ const myButtonClickHandler = function () {
 };
 
 myButton.addEventListener('click', myButtonClickHandler);
+
+
